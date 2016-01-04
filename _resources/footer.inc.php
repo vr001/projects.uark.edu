9789eee15f57a2a1dc6b668c679123efaf0dec17
administrator@Waltahr <jpuckett@walton.uark.edu>
2016-01-04 17:00:58 -0600

    add uark footer

--- _resources/footer.inc.php
+++ _resources/footer.inc.php
@@ -10,6 +10,56 @@ if ( ! ((strpos(basename($_SERVER["SCRIPT_NAME"]),'.ajax.') !== false) || (strpo
 		</div><!-- /#page-content-container -->
 
 	    </div><!-- /#page-content-wrapper -->
+	    
+<section class="gray" id="section-footer" style="padding: 10px 0px 20px 0px;">
+         <div class="container">
+            <div class="row" style="padding-top: 30px;">
+               <div class="col-sm-7">
+<h3><a href="http://walton.uark.edu" style='color:#5a5a5a;'>Sam M. Walton College of Business</a></h3>
+
+
+<p style="margin-bottom: 35px; padding: 20px;"><a href="//walton.uark.edu/directory/" class="btn navbar-btn btn-default" style="margin-top: -15px;">Walton Directory</a> <a href="https://intranet.waltoncollege.uark.edu:8001" class="btn navbar-btn btn-default" style="margin-top: -15px; margin-right: 10px;">Walton Intranet</a> <span style="display:inline-block;"><a style="color: #b7b7b7" title=”facebook” href="https://www.facebook.com/WaltonCollege"><i class="fa fa-3x fa-facebook-square"><!-- content --></i></a> <a style="color: #b7b7b7" title="twitter" href="https://twitter.com/uawaltoncollege"><i class="fa fa-3x fa-twitter-square"><!-- content --></i></a> <a style="color: #b7b7b7" title="Linkedin" href="https://www.linkedin.com/grp/home?gid=108950"><i class="fa fa-3x fa-fa fa-linkedin-square"><!-- content --></i></a> <a style="color: #b7b7b7" title="instagram" href="https://instagram.com/uawaltoncollege/"><i class="fa fa-3x fa-instagram"><!-- content --></i></a></span></p>
+</div>
+               <div class="col-sm-5">
+                  <div class="row">
+                     <div class="col-md-8"><a href="http://walton.uark.edu"><img alt="Sam M. Walton College of Business at the University of Arkansas" src="<?php echo $path_web_root; ?>/_resources/images/WaltonLogo2.png" class="img-responsive"></a></div>
+                     <div class="col-md-4 hidden-xs hidden-sm visible-md visible-lg"><a href="http://accredited.aacsb.edu/students"><img alt="AACSB Accredited" src="<?php echo $path_web_root; ?>/_resources/images/aacsb-large.png" width="120px" class="img-responsive" style="filter: alpha(opacity=30); opacity: 0.3"></a></div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </section>
+
+<!-- FOOTER -->
+<footer>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+	<h3 id="footer-logo"><a href="http://uark.edu">UNIVERSITY OF ARKANSAS</a></h3>
+	<ul id="footer-global-links" class="list-unstyled">
+	  <li><a href="http://www.uark.edu/admissions/index.php">Admissions</a></li>
+	  <li><a href="http://www.uark.edu/academics/index.php">Academics</a></li>
+	  <li><a href="http://www.uark.edu/campus-life/index.php">Campus Life</a></li>
+	  <li><a href="http://www.uark.edu/research/index.php">Research</a></li>
+	  <li><a href="http://www.uark.edu/athletics/index.php">Athletics</a></li>
+	  <li><a href="http://www.uark.edu/about/index.php">About</a></li>
+	</ul>
+	<ul id="social-stack" class="nav pull-right clearfix list-unstyled">
+	    <li><a href="https://www.facebook.com/UofArkansas"><i class="fa fa-facebook"></i> <span class="sr-only">Like us on Facebook</span> </a></li>
+	    <li><a href="http://twitter.com/uarkansas"><i class="fa fa-twitter"></i> <span class="sr-only">Follow us on Twitter</span></a></li>
+	    <li><a href="http://www.youtube.com/user/UniversityArkansas"><i class="fa fa-youtube"></i> <span class="sr-only">Watch us on YouTube</span></a></li>
+	    <li><a href="http://instagram.com/uarkansas"><i class="fa fa-instagram"></i> <span class="sr-only">See us on Instagram</span></a></li>
+	    <li><a href="https://plus.google.com/104159281704656057709" rel="publisher"><i class="fa fa-google-plus"></i> <span class="sr-only">Connect with us on Google+</span></a></li>	
+	    <li><a href="http://pinterest.com/uofaadmissions/"><i class="fa fa-pinterest"></i> <span class="sr-only">Join us on Pinterest</span></a></li>
+	    <li><a href="http://www.linkedin.com/company/university-of-arkansas"><i class="fa fa-linkedin"></i> <span class="sr-only">Connect with us on LinkedIn</span></a></li>
+	    <li><a href="https://foursquare.com/uarkansas"><i class="fa fa-foursquare"></i> <span class="sr-only">Find us on FourSquare</span></a></li>
+	</ul>
+      </div>
+    </div>
+  </div>
+</footer><!-- /FOOTER -->
+
+
 
 
     </div><!-- /#wrapper -->
