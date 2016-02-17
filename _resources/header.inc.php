5f14dbec1c95be6fa0e830ad7831b76c8b34e489
Jeff Puckett <jeff@jeffpuckett.com>
2016-02-16 22:08:50 -0600

    fix root header h1 link

--- _resources/header.inc.php
+++ _resources/header.inc.php
@@ -59,8 +59,8 @@ if ( ! ((strpos(basename($_SERVER["SCRIPT_NAME"]),'.ajax.') !== false) || (strpo
 <div id="header-bg"></div>
 <div class="container" id="opener" role="banner">
   <a class="brand" href="http://www.uark.edu/">University of Arkansas</a>
-  <h1 id="site-heading" style="position: absolute; top: 27px; margin-left: 220px;"><span class="walton-name"><a href="<?php echo $path_web_root;?>" style='color: #505050; font-weight: 700; font-size: 40px;'><?php echo $site_title;?></a></span></h1>
-  <p id="site-heading" style="position: absolute; top: 85px; margin-left: 220px; font-size: 20px;"><span class="walton-sub-name"><a href="<?php echo $path_web_root;?>" style='color: #b3b3b3'>Stakeholder Engagement</a></span></p>
+  <h1 id="site-heading" style="position: absolute; top: 27px; margin-left: 220px;"><span class="walton-name"><a href="<?php echo "$path_web_root/";?>" style='color: #505050; font-weight: 700; font-size: 40px;'><?php echo $site_title;?></a></span></h1>
+  <p id="site-heading" style="position: absolute; top: 85px; margin-left: 220px; font-size: 20px;"><span class="walton-sub-name"><a href="<?php echo "$path_web_root/";?>" style='color: #b3b3b3'>Stakeholder Engagement</a></span></p>
 </div><!-- /#opener banner -->
 
     <nav class="navbar navbar-default" style='margin-top: 60px'>
