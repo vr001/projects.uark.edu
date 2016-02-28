652bd0913c900b44306fe31e4a83afbe239bfb6c
Jeff Puckett <jeff@jeffpuckett.com>
2016-02-28 15:34:16 -0600

    complete add collaborator script

--- _resources/_setup/create_collaborator.bash
+++ _resources/_setup/create_collaborator.bash
@@ -27,7 +27,16 @@ EOF
 # TODO: create git ssh keys
 
 # add database access
-read -e -p "enter mysql root password: " mysql_root_password
+mysql_root_password=""
+# move to working directory
+cd $( dirname "${BASH_SOURCE[0]}" )
+# trump credentials if external file exists
+if [ -f credentials_local.bash ]; then
+  source credentials_local.bash
+fi
+if [[ -z "$mysql_root_password" ]]; then
+  read -e -p "enter mysql root password: " mysql_root_password
+fi
 if [[ -z "$mysql_root_password" ]]; then
   echo "Error: mysql_root_password cannot be empty"
   echo "Exiting"; exit 1
@@ -42,6 +51,8 @@ CREATE USER '$new_collaborator'@'$database_server' IDENTIFIED BY '$database_pass
 GRANT ALL PRIVILEGES ON $database_name.* TO '$new_collaborator'@'$database_server';
 CREATE USER '$new_collaborator'@'%' IDENTIFIED BY '$database_password';
 GRANT ALL PRIVILEGES ON $database_name.* TO '$new_collaborator'@'%';
+GRANT SELECT ON *.* TO '$new_collaborator'@'$database_server';
+GRANT SELECT ON *.* TO '$new_collaborator'@'%';
 EOF
 echo "
 mysql database name: $database_name
@@ -49,4 +60,32 @@ mysql username: $new_collaborator
 mysql password: $database_password
 "
 
-# TODO: create virtual host
+# create virtual host
+cd /var/www/test/
+git clone https://github.com/admonkey/projects.uark.edu.git
+cd projects.uark.edu/
+git checkout -b $new_collaborator
+# create local credentials files
+# php
+echo "
+<?php
+\$database_username='$new_collaborator';
+\$database_password='$database_password';
+\$database_name='$database_name';
+?>
+" > _resources/credentials_local.inc.php
+# bash
+echo "
+database_user='$new_collaborator'
+database_password='$database_password'
+database_name='$database_name'
+" > _resources/SQL/credentials_local.bash
+cd ..
+mv projects.uark.edu/ $new_collaborator/
+chown -R $new_collaborator:$new_collaborator $new_collaborator/
+
+# copy error log alias to .bashrc
+echo 'alias elog="cat /var/log/httpd/test.projects.uark.edu.ssl-error_log"' >> /home/$new_collaborator/.bashrc
+
+# move to virtual host directory
+echo "alias www='cd /var/www/test/$new_collaborator'" >> /home/$new_collaborator/.bashrc
