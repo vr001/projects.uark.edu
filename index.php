49398da0dcea26adcb745cb6651fc9383844431a
Jeff Puckett <jeff@jeffpuckett.com>
2016-03-01 21:59:24 -0600

    illustrate pull

--- index.php
+++ index.php
@@ -6,8 +6,14 @@ $no_well_container = true;
 // $section_title = "Root Section";
 require_once('_resources/header.inc.php');
 
+$site_owner = get_current_user();
+
 echo "
   <h1>Welcome to $site_title</h1>
+  
+  <div class='well'>
+    <p>$site_owner has pulled down changes from github.</p>
+  </div>
 ";
 
 require_once('_resources/footer.inc.php');
