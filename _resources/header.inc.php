e1655f8e8ab9e18e79cd1f7e612f4c018d446e2b
Jeff Puckett <jeff@jeffpuckett.com>
2016-03-05 16:10:01 -0600

    fix root href in site header

--- _resources/header.inc.php
+++ _resources/header.inc.php
@@ -75,7 +75,7 @@ if ( ! ((strpos(basename($_SERVER["SCRIPT_NAME"]),'.ajax.') !== false) || (strpo
 <div id="header-bg"></div>
 <div class="container" id="opener" role="banner">
   <a class="brand" href="http://www.uark.edu/">University of Arkansas</a>
-  <h1 id="site-heading" style="position: absolute; top: 27px; margin-left: 220px;"><span class="walton-name"><a href="<?php echo $path_web_root;?>"><?php echo $site_title;?></a></span></h1>
+  <h1 id="site-heading" style="position: absolute; top: 27px; margin-left: 220px;"><span class="walton-name"><a href="<?php echo $path_web_root;?>/"><?php echo $site_title;?></a></span></h1>
   <h4>&nbsp;</h4>
   <p id="site-heading" style="position: absolute; top: 85px; margin-left: 220px; font-size: 20px;"><span class="walton-sub-name"><a href="//walton.uark.edu">The Sam M. Walton College of Business</a></span></p>
 </div><!-- /#opener banner -->
