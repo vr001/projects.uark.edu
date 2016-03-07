d1f2a98ecd99cd8086d29c8426cf5f25cfc45b3f
vr001 <villan.lampard.epl@gmail.com>
2016-03-07 13:00:46 -0600

    Update projects.seed.sql
    Created some new users

--- _resources/SQL/projects.seed.sql
+++ _resources/SQL/projects.seed.sql
@@ -4,7 +4,6 @@ INSERT INTO Users (email,username) VALUES ('vr001','Vignesh Ravichandran');
 INSERT INTO Users (email,username) VALUES ('gw004','Greg Williams');
 INSERT INTO Users (email,username) VALUES ('naliu','Luna');
 INSERT INTO Users (email,username) VALUES ('dbruce','David Bruce');
-Insert INTO Users (
-INSERT INTO Users (email,username) VALUES ('dbruce','David Bruce');
-INSERT INTO Users (email,username) VALUES ('dbruce','David Bruce');
-INSERT INTO Users (email,username) VALUES ('dbruce','David Bruce');
+INSERT INTO Users (email,username) VALUES ('schabba','Sukhmani Chabba');
+INSERT INTO Users (email,username) VALUES ('dkhan','Danish Khan');
+
